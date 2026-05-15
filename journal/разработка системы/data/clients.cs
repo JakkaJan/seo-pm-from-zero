3079b3261afc324d4e3019453fa253fdf90f02d5
JakkaJan <79934874+JakkaJan@users.noreply.github.com>
2026-05-16 02:56:45 +0500

    Create clients.cs

---
+++ journal/разработка системы/data/clients.cs
@@ -0,0 +1,9 @@
+client_id,client_name,site_url,cms,launch_date,monthly_traffic,seo_package,account_manager,last_audit_date
+C001,АвтоЦентр Юг,https://autocenter-yug.ru,WordPress,2024-03-15,1200,Базовый,Иванова М.,2026-04-10
+C002,ТЦ МегаМолл,https://megamall-ufa.ru,1C-Bitrix,2023-11-20,8500,Премиум,Петров А.,2026-03-22
+C003,Завод Прогресс,https://progress-plant.ru,ModX,2025-01-10,340,Базовый,Сидорова Е.,2026-05-01
+C004,IT-Сервис Про,https://itservice-pro.ru,Tilda,2024-08-05,2100,Стандарт,Иванова М.,2026-04-15
+C005,МедЦентр Здоровье,https://medcenter-health.ru,WordPress,2023-05-18,5600,Премиум,Петров А.,2026-02-28
+C006,СтройГрупп,https://stroygroup-ufa.ru,1C-Bitrix,2024-12-01,890,Стандарт,Сидорова Е.,2026-04-05
+C007,ФитнесКлуб Лайт,https://fitness-lite.ru,WordPress,2025-02-14,150,Базовый,Иванова М.,2026-01-20
+C008,Кафе Березка,https://cafe-berezka.ru,Tilda,2024-06-30,2300,Стандарт,Петров А.,2026-03-10
